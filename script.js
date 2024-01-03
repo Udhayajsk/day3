7f7c0b47d2da49b290c4cf5b8e3783d1a78a1431
Udhayajsk <154896176+Udhayajsk@users.noreply.github.com>
2024-01-03 20:55:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var request=new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all");
+request.send();
+request.onload=function(){
+    var result =JSON.parse(request.response);
+    console.log(result);
+    for(var i=0;i<result.length;i++){
+        console.log("name:" + result[i].name);
+        console.log("region:" + result[i].region);
+        console.log("subregion:" + result[i].subregion);
+        console.log("population:" + result[i].population);
+    
+    }
+    
+}
+    
---
+++ script2.js
@@ -0,0 +1,14 @@
+let obj1={
+    name:"person 1",
+    age:5};
+let obj2={
+    age:5,
+    name:"person 1"
+};
+
+if(obj1==obj2){
+    console,log("true");
+}else{
+    console.log("false");
+}
+
